7e277b2011132ae5105ec07d31ead0c322d02ff3
Kimmie George <kimmiegeorge@Kimmies-Mac-mini.local>
2022-06-13 09:23:09 -0700

    Commit

---
+++ CreateReturnMeasures.R
@@ -0,0 +1,9 @@
+# Create return measures - 3 month CAR and 5 day CAR 
+
+library(pacman)
+p_load(data.table)
+
+
+# load data 
+crsp <- fread('/Volumes/Elements/Research/Reddit_Credibility/Data/CRSP_2021.csv')
+
